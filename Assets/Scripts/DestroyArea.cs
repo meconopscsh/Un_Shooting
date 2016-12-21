3db66a39f048dbed4d783cc0b5dac47f9388bbdb
meconopscsh <yhwh3333@gmail.com>
2016-12-21 22:38:41 +0900

    test

---
+++ Assets/Scripts/DestroyArea.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyArea : MonoBehaviour {
+
+    private void OnTriggerExit2D(Collider2D c)
+    {
+        Destroy(c.gameObject);
+    }
+}
---
+++ Assets/Scripts/Emitter.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Emitter : MonoBehaviour {
+
+    // Waveプレハブを格納する
+    public GameObject[] waves;
+
+    // 現在のWave
+    private int currentWave;
+
+    private IEnumerator Start()
+    {
+
+        // Waveが存在しなければコルーチンを終了する
+        if (waves.Length == 0) {
+            yield break;
+        }
+
+        while (true) {
+
+            // Waveを作成する
+            GameObject wave = (GameObject)Instantiate(waves[currentWave], transform.position, Quaternion.identity);
+
+            //WaveをEmitterの子要素にする
+            wave.transform.parent = transform;
+
+            // Waveの子要素のEnemyが全て削除されるまで待機する
+            while (wave.transform.childCount != 0) {
+                yield return new WaitForEndOfFrame();
+            }
+
+            //Waveの削除
+            Destroy(wave);
+
+            //格納されているWaveを全て実行したらCurrentWaveを０にする　（最初から　->　ループ）
+            if (waves.Length <= ++currentWave) {
+                currentWave = 0;
+            }
+
+        }
+    }
+ 
+	
+}
